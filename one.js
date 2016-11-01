1ea94499a8bb47c9258911a9a517c25b462bea64
nikitha <msainikitha@gmail.com>
2016-11-01 16:04:42 +0530

    implemented search and pagination functionality

--- one.js
+++ one.js
@@ -1,4 +1,4 @@
-var app = angular.module("myApp", ["ngRoute"]);
+var app = angular.module("myApp", ["ngRoute","angularUtils.directives.dirPagination"]);
 app.config(function($routeProvider) {
     $routeProvider
     .when("/home", {
